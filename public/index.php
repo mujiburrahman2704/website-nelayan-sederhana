38b9ae8aee15fb927ba454c96c4ecae6b91f483e
mujiburrahman <mujiburrahman2704@gmail.com>
2022-05-28 02:16:50 +0700

    update

---
+++ public/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/output.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="fotobg">
+    <div class="container mx-auto">
+        <div class="lg:grid w-full lg:grid-cols-6 lg:gap-4 mx-4">
+            <div class="lg:col-end-7 lg:col-span-2 w-full">
+                <div class="flex flex-col lg:py-32 mx-auto">
+                    <div class="border rounded shadow-md shadow-white bg-white">
+                        <div class="text-center border-b-2 font-sans py-2 border-black font-extrabold">
+                            <h2>SISTEM INFORMASI NELAYAN</h2>
+                            <h2>DESA PUSONG LAMA</h2>
+                            <H2>KOTA LHOKSEUMAWE</H2>
+                        </div>
+                        <div class="mx-4 text-black py-6 space-y-2">
+                            <h2 class="font-sans font-medium">Username</h2>
+                            <div class="mx-6">
+                                <input type="text" class="w-full px-2 hover:bg-gray-300 py-1 rounded border-2 " placeholder=":">
+                            </div>
+                            <h2 class="font-sans font-medium">Password</h2>
+                            <div class="mx-6">
+                                <input type="password" class="px-2 w-full rounded border-2 py-1" placeholder=":">
+                            </div>
+                        </div>
+                        <div class="mx-4 flex justify-end space-x-2 py-4">
+                            <button class="border rounded-lg bg-blue-500 text-white font-medium py-1 px-6">Login</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
